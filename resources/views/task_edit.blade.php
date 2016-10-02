311ccc4d6d4aa2b6ca64b3481fd81d5c9f0bf549
Nuttapol Nonseeveerachai <nuttapol.nonsee@gmail.com>
2016-10-02 16:44:29 +0700

    Create task edit view.

---
+++ resources/views/task_edit.blade.php
@@ -0,0 +1,38 @@
+<!-- resources/views/tasks.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+
+	<!-- Bootstrap Boilerplate... -->
+
+	<div class="panel-body">
+		<!-- Display Validation Errors -->
+		
+		@include('common.errors')
+		<!-- New Task Form -->
+		<form action="{{ URL::full() }}" method="POST" class="form-horizontal">
+			{{ csrf_field() }} 
+			{{ method_field('PUT') }}
+
+			<!-- Task Name -->
+			<div class="form-group">
+				<label for="task" class="col-sm-3 control-label">Task</label>
+
+				<div class="col-sm-6">
+					<input type="text" name="name" id="task-name" class="form-control" value="{{ $task->name }}">
+				</div>
+			</div>
+
+			<!-- Edit Task Button -->
+			<div class="form-group">
+				<div class="col-sm-offset-3 col-sm-6">
+					<button type="submit" class="btn btn-default">
+						<i class="fa fa-plus"></i> Edit Task
+					</button>
+				</div>
+			</div>
+		</form>
+	</div>
+	
+@endsection
